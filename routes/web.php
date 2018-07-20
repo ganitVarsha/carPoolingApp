7607042dfdae3cae9492b0ea10d9699b099e5f93
Varsha Mittal <varsha.mittal@ganitsoft.com>
2018-07-20 15:37:02 +0530

    permission error

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('admin', function () {
+
+    return view('admin');
+
+});
